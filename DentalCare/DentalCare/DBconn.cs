bd3e17349d05f83304d8aff579ae55ccfb844c0a
GiannisFT <fltsouri@gmail.com>
2018-11-02 09:55:49 +0100

    feature/1

---
+++ DentalCare/DentalCare/DBconn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DentalCare
+{
+    class DBconn
+    {
+        DataTable dt;
+        SqlCommand myCommand;
+        SqlDataAdapter adapter;
+        SqlConnection myConnection;
+        SqlParameter parameter1 = new SqlParameter();
+        SqlParameter parameter2 = new SqlParameter();
+
+        public DBconn()
+        {
+            myConnection = new SqlConnection();
+            myConnection.ConnectionString = "Integrated Security=true;database=dbDentalCare;Data Source=LAPTOP-7DKPE6B0\\SQLEXPRESS14";
+        }
+
+        public DataTable Login(string username,string password,string role)
+        {
+
+            myCommand = new SqlCommand("select L.fldUserName,L.fldPassword,U.fldRole from tblLogin L inner join tblUser U on L.fldLoginId=U.fldLoginId where fldUserName='" + username 
+                                        + "'and fldPassword='" + password + "'and fldRole='" + role + "'",myConnection);
+
+            dt = new DataTable();
+            adapter = new SqlDataAdapter(myCommand);
+            adapter.Fill(dt);
+            return dt;
+        }
+    }
+}
