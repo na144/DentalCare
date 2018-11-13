e480cf9474d4a3cea96a94e2d672fe393e7e65bd
knnv <karyn.hvo@gmail.com>
2018-11-13 15:15:09 +0100

    added stored proc. and method to getClientInfo

---
+++ DentalCare/DentalCare/ClientInfoView.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace DentalCare
+{
+    partial class ClientInfoView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlNavDent = new System.Windows.Forms.Panel();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.lblReceptionistUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReceptionistMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.mainPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.appointmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewBookingStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.patientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewPatientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seePatientListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dentalStaffsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlPatInfo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txbPersNum = new System.Windows.Forms.TextBox();
+            this.txbPhNum = new System.Windows.Forms.TextBox();
+            this.txbFname = new System.Windows.Forms.TextBox();
+            this.txbPcode = new System.Windows.Forms.TextBox();
+            this.txbLname = new System.Windows.Forms.TextBox();
+            this.txbCity = new System.Windows.Forms.TextBox();
+            this.txbAdr = new System.Windows.Forms.TextBox();
+            this.txbSearchClient = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearchPatient = new System.Windows.Forms.Button();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.pnlNavDent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.ReceptionistMenuStrip.SuspendLayout();
+            this.pnlPatInfo.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlNavDent
+            // 
+            this.pnlNavDent.BackColor = System.Drawing.Color.White;
+            this.pnlNavDent.Controls.Add(this.picBox);
+            this.pnlNavDent.Controls.Add(this.lblReceptionistUser);
+            this.pnlNavDent.Controls.Add(this.label1);
+            this.pnlNavDent.Controls.Add(this.ReceptionistMenuStrip);
+            this.pnlNavDent.Location = new System.Drawing.Point(-4, 3);
+            this.pnlNavDent.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlNavDent.Name = "pnlNavDent";
+            this.pnlNavDent.Size = new System.Drawing.Size(877, 137);
+            this.pnlNavDent.TabIndex = 1;
+            // 
+            // picBox
+            // 
+            this.picBox.Image = global::DentalCare.Properties.Resources.Logo7;
+            this.picBox.Location = new System.Drawing.Point(13, 9);
+            this.picBox.Margin = new System.Windows.Forms.Padding(4);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(88, 80);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // lblReceptionistUser
+            // 
+            this.lblReceptionistUser.AutoSize = true;
+            this.lblReceptionistUser.Location = new System.Drawing.Point(676, 10);
+            this.lblReceptionistUser.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblReceptionistUser.Name = "lblReceptionistUser";
+            this.lblReceptionistUser.Size = new System.Drawing.Size(46, 17);
+            this.lblReceptionistUser.TabIndex = 2;
+            this.lblReceptionistUser.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(577, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Signed in as:";
+            // 
+            // ReceptionistMenuStrip
+            // 
+            this.ReceptionistMenuStrip.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ReceptionistMenuStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.ReceptionistMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ReceptionistMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainPageToolStripMenuItem,
+            this.appointmentsToolStripMenuItem,
+            this.patientsToolStripMenuItem,
+            this.dentalStaffsToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.logoutToolStripMenuItem});
+            this.ReceptionistMenuStrip.Location = new System.Drawing.Point(4, 107);
+            this.ReceptionistMenuStrip.MinimumSize = new System.Drawing.Size(867, 30);
+            this.ReceptionistMenuStrip.Name = "ReceptionistMenuStrip";
+            this.ReceptionistMenuStrip.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.ReceptionistMenuStrip.Size = new System.Drawing.Size(867, 30);
+            this.ReceptionistMenuStrip.TabIndex = 0;
+            this.ReceptionistMenuStrip.Text = "menuStrip1";
+            // 
+            // mainPageToolStripMenuItem
+            // 
+            this.mainPageToolStripMenuItem.Name = "mainPageToolStripMenuItem";
+            this.mainPageToolStripMenuItem.Size = new System.Drawing.Size(90, 26);
+            this.mainPageToolStripMenuItem.Text = "Main Page";
+            // 
+            // appointmentsToolStripMenuItem
+            // 
+            this.appointmentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.NewBookingStripMenuItem});
+            this.appointmentsToolStripMenuItem.Name = "appointmentsToolStripMenuItem";
+            this.appointmentsToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.appointmentsToolStripMenuItem.Text = "Appointments";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // NewBookingStripMenuItem
+            // 
+            this.NewBookingStripMenuItem.Name = "NewBookingStripMenuItem";
+            this.NewBookingStripMenuItem.Size = new System.Drawing.Size(173, 26);
+            this.NewBookingStripMenuItem.Text = "New booking";
+            // 
+            // patientsToolStripMenuItem
+            // 
+            this.patientsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewPatientToolStripMenuItem,
+            this.seePatientListToolStripMenuItem});
+            this.patientsToolStripMenuItem.Name = "patientsToolStripMenuItem";
+            this.patientsToolStripMenuItem.Size = new System.Drawing.Size(72, 26);
+            this.patientsToolStripMenuItem.Text = "Patients";
+            // 
+            // addNewPatientToolStripMenuItem
+            // 
+            this.addNewPatientToolStripMenuItem.Name = "addNewPatientToolStripMenuItem";
+            this.addNewPatientToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.addNewPatientToolStripMenuItem.Text = "Add New Patient";
+            // 
+            // seePatientListToolStripMenuItem
+            // 
+            this.seePatientListToolStripMenuItem.Name = "seePatientListToolStripMenuItem";
+            this.seePatientListToolStripMenuItem.Size = new System.Drawing.Size(195, 26);
+            this.seePatientListToolStripMenuItem.Text = "See Patient List";
+            // 
+            // dentalStaffsToolStripMenuItem
+            // 
+            this.dentalStaffsToolStripMenuItem.Name = "dentalStaffsToolStripMenuItem";
+            this.dentalStaffsToolStripMenuItem.Size = new System.Drawing.Size(106, 26);
+            this.dentalStaffsToolStripMenuItem.Text = "Dental Staffs";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(74, 26);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(68, 26);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // pnlPatInfo
+            // 
+            this.pnlPatInfo.Controls.Add(this.panel1);
+            this.pnlPatInfo.Controls.Add(this.txbSearchClient);
+            this.pnlPatInfo.Controls.Add(this.textBox1);
+            this.pnlPatInfo.Controls.Add(this.btnSearchPatient);
+            this.pnlPatInfo.Location = new System.Drawing.Point(0, 141);
+            this.pnlPatInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlPatInfo.Name = "pnlPatInfo";
+            this.pnlPatInfo.Size = new System.Drawing.Size(867, 432);
+            this.pnlPatInfo.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txbEmail);
+            this.panel1.Controls.Add(this.txbPersNum);
+            this.panel1.Controls.Add(this.txbPhNum);
+            this.panel1.Controls.Add(this.txbFname);
+            this.panel1.Controls.Add(this.txbPcode);
+            this.panel1.Controls.Add(this.txbLname);
+            this.panel1.Controls.Add(this.txbCity);
+            this.panel1.Controls.Add(this.txbAdr);
+            this.panel1.Location = new System.Drawing.Point(41, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 237);
+            this.panel1.TabIndex = 15;
+            // 
+            // txbPersNum
+            // 
+            this.txbPersNum.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbPersNum.Location = new System.Drawing.Point(38, 14);
+            this.txbPersNum.Multiline = true;
+            this.txbPersNum.Name = "txbPersNum";
+            this.txbPersNum.Size = new System.Drawing.Size(154, 23);
+            this.txbPersNum.TabIndex = 7;
+            // 
+            // txbPhNum
+            // 
+            this.txbPhNum.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbPhNum.Location = new System.Drawing.Point(38, 152);
+            this.txbPhNum.Multiline = true;
+            this.txbPhNum.Name = "txbPhNum";
+            this.txbPhNum.Size = new System.Drawing.Size(154, 23);
+            this.txbPhNum.TabIndex = 14;
+            // 
+            // txbFname
+            // 
+            this.txbFname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbFname.Location = new System.Drawing.Point(38, 37);
+            this.txbFname.Multiline = true;
+            this.txbFname.Name = "txbFname";
+            this.txbFname.Size = new System.Drawing.Size(154, 23);
+            this.txbFname.TabIndex = 9;
+            // 
+            // txbPcode
+            // 
+            this.txbPcode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbPcode.Location = new System.Drawing.Point(38, 129);
+            this.txbPcode.Multiline = true;
+            this.txbPcode.Name = "txbPcode";
+            this.txbPcode.Size = new System.Drawing.Size(154, 23);
+            this.txbPcode.TabIndex = 13;
+            // 
+            // txbLname
+            // 
+            this.txbLname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbLname.Location = new System.Drawing.Point(38, 60);
+            this.txbLname.Multiline = true;
+            this.txbLname.Name = "txbLname";
+            this.txbLname.Size = new System.Drawing.Size(154, 23);
+            this.txbLname.TabIndex = 10;
+            // 
+            // txbCity
+            // 
+            this.txbCity.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbCity.Location = new System.Drawing.Point(38, 106);
+            this.txbCity.Multiline = true;
+            this.txbCity.Name = "txbCity";
+            this.txbCity.Size = new System.Drawing.Size(154, 23);
+            this.txbCity.TabIndex = 12;
+            // 
+            // txbAdr
+            // 
+            this.txbAdr.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbAdr.Location = new System.Drawing.Point(38, 83);
+            this.txbAdr.Multiline = true;
+            this.txbAdr.Name = "txbAdr";
+            this.txbAdr.Size = new System.Drawing.Size(154, 23);
+            this.txbAdr.TabIndex = 11;
+            // 
+            // txbSearchClient
+            // 
+            this.txbSearchClient.Location = new System.Drawing.Point(426, 38);
+            this.txbSearchClient.Name = "txbSearchClient";
+            this.txbSearchClient.Size = new System.Drawing.Size(161, 22);
+            this.txbSearchClient.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(439, 161);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 170);
+            this.textBox1.TabIndex = 6;
+            // 
+            // btnSearchPatient
+            // 
+            this.btnSearchPatient.Location = new System.Drawing.Point(606, 35);
+            this.btnSearchPatient.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearchPatient.Name = "btnSearchPatient";
+            this.btnSearchPatient.Size = new System.Drawing.Size(100, 28);
+            this.btnSearchPatient.TabIndex = 2;
+            this.btnSearchPatient.Text = "Enter";
+            this.btnSearchPatient.UseVisualStyleBackColor = true;
+            this.btnSearchPatient.Click += new System.EventHandler(this.btnSearchPatient_Click);
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbEmail.Location = new System.Drawing.Point(38, 175);
+            this.txbEmail.Multiline = true;
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(154, 23);
+            this.txbEmail.TabIndex = 15;
+            // 
+            // ClientInfoView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 571);
+            this.Controls.Add(this.pnlPatInfo);
+            this.Controls.Add(this.pnlNavDent);
+            this.Name = "ClientInfoView";
+            this.Text = "PatientInfoView";
+            this.pnlNavDent.ResumeLayout(false);
+            this.pnlNavDent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ReceptionistMenuStrip.ResumeLayout(false);
+            this.ReceptionistMenuStrip.PerformLayout();
+            this.pnlPatInfo.ResumeLayout(false);
+            this.pnlPatInfo.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlNavDent;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label lblReceptionistUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip ReceptionistMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mainPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem appointmentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NewBookingStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem patientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewPatientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seePatientListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dentalStaffsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlPatInfo;
+        private System.Windows.Forms.Button btnSearchPatient;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbSearchClient;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txbPersNum;
+        private System.Windows.Forms.TextBox txbPhNum;
+        private System.Windows.Forms.TextBox txbFname;
+        private System.Windows.Forms.TextBox txbPcode;
+        private System.Windows.Forms.TextBox txbLname;
+        private System.Windows.Forms.TextBox txbCity;
+        private System.Windows.Forms.TextBox txbAdr;
+        private System.Windows.Forms.TextBox txbEmail;
+    }
+}
--- DentalCare/DentalCare/PatientHandling.cs
+++ DentalCare/DentalCare/PatientHandling.cs
@@ -39,5 +39,7 @@ namespace DentalCare
             }
             return false;
         }
+
+        
     }
 }
